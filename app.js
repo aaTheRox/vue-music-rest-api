03c14b5377dad2438da2aa990f7adc8273ae5350
nouaryk <nouary@live.com>
2019-12-20 17:06:53 +0100

    added new features
    Improved getPlaylists
    Delete songs in a playlist

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000
 const fs = require('fs');
 var express = require("express"),
     session = require('express-session')
 app = express(),
     bodyParser = require("body-parser"),
     methodOverride = require("method-override");
-
+var bcrypt = require('bcryptjs');
 // MONGOOSE CONNECTION
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/vue-music', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -31,18 +31,29 @@ app.use(function(req, res, next) {
 var router = express.Router();
 app.use(router);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Node server running on http://localhost:3000");
+/*var salt = bcrypt.genSaltSync(10);
+var hash = bcrypt.hashSync("raider/\/", salt);
+    console.log(hash)
+    console.log(rypt.compareSync("B4c0/\/", hash))*/
 });
+router.get('/', async(req, res) => {
+    const playlist = new Playlist({title: 'Ok'});
+    await playlist.save()
 
-
+})
 // PLAYLIST
 router.post('/playlist/add', async(req, res) => {
-
-    const playlist = new Playlist(req.body.data);
-    await playlist.save()
-    console.log("[REST API] Se añadido una nueva playlist.");
-    res.send({ status: 'PLAYLIST_CREATED_SUCCESS' });
+    try {
+        const playlist = new Playlist(req.body.data);
+        await playlist.save()
+        console.log("[REST API] Se añadido una nueva playlist.");
+        res.send({ status: 'PLAYLIST_CREATED_SUCCESS' });
+    } catch (error) {
+        console.log("[REST API] ERROR: No se pudo crear la playlist.");
+        res.send({ status: 'PLAYLIST_CREATED_FAILED' });
+    }
 });
 
 router.post('/add-song-toplaylist', async(req, res) => {
@@ -82,10 +93,70 @@ router.get('/test', async(req, res) => {
     res.send(playlist)
 });
 
-router.get('/getPlaylists', async(req, res) => {
-    const playlists = await Playlist.find();
-    res.send(playlists);
-    console.log("[REST API] Playlists cargadas con éxito.");
+router.post('/getPlaylists', async(req, res) => {
+    console.log("[REST API] Buscando playlists...");
+    const pagination = req.body.pagination;
+    
+    try
+    {
+        let playlists, total;
+
+        const ITEMS_PER_PAGE = pagination.perPage;
+        const PAGE_NUMBER = pagination.PAGE_NUMBER;
+        const NUMBER_OF_ITEMS = pagination.NUMBER_OF_ITEMS;
+
+        total = await Playlist.countDocuments();
+        playlists = await Playlist.find().skip(ITEMS_PER_PAGE * (PAGE_NUMBER - 1)).limit(ITEMS_PER_PAGE )
+        res.send({playlists: playlists, total: total});
+
+        console.log("[REST API] Playlists cargadas con éxito.");
+    } catch(error) {
+        console.log("[REST API] Ha ocurrido un error al intentar obtener las playlists.", error);
+    }
+   
+});
+
+router.post('/getAllPlaylists', async(req, res) => {
+    try
+    {
+        const playlists = await Playlist.find();
+        const total = await Playlist.countDocuments();
+           
+        res.send({playlists: playlists, total: total});
+        console.log("[REST API] Playlists cargadas con éxito.");
+    } catch(error) {
+        console.log("[REST API] Ha ocurrido un error al intentar obtener las playlists.", error);
+    }
+});
+
+router.get('/getPlaylist/:id', async(req, res) => {
+    try {
+        const playlistId = {_id: req.params.id};
+        const playlist = await Playlist.find(playlistId);
+        res.send(playlist);
+        console.log("[REST API] Playlist cargada con éxito.");
+    } catch (error) {
+        res.send({status: "PLAYLIST_NOT_FOUND"});
+        console.log("[REST API] No se pudo cargar la playlist.");
+    }
+});
+
+router.post('/playlist/delete/song', async(req, res) => {
+    const playlistId = req.body.data.playlistId;
+    try {
+        const playlist = await Playlist.findByIdAndUpdate(playlistId, {
+            $pull: {
+                songs: req.body.data.songId
+            }
+        });
+           
+        console.log(`[REST API] Se eliminado la playlist: ${playlistId}.`);
+        res.send({ status: 'PLAYLIST_SONG_DELETED_SUCCESS' });
+    } catch (error) {
+        console.log('[REST API] Playlist NO encontrada');
+        console.log(error)
+        res.send({ status: 'PLAYLIST_NOT_FOUND' });
+    }
 });
 
 router.post('/playlist/delete/:id', async(req, res) => {
@@ -105,7 +176,6 @@ router.post('/playlist/delete/:id', async(req, res) => {
 
 router.post('/playlist/edit/:id', async(req, res) => {
 
-    console.log('NEW TITLE: ', req.body.data.title);
     const playlist = Playlist.find({ _id: req.params.id })
     await playlist.updateOne({ _id: req.params.id }, {
         $set: {
@@ -124,8 +194,7 @@ router.post('/playlist/edit/:id', async(req, res) => {
 router.post('/register', async(req, res) => {
     const data = req.body.data;
     const user = await Users.find({
-        email: data.email,
-        password: data.password
+        email: data.email
     });
     if (req.body.data.email == '' || req.body.data.email == '') {
         res.send({ status: 'USER_CREATED_FAILED' });
@@ -182,7 +251,11 @@ router.post('/login', async(req, res) => {
         console.log("[REST API] El usuario no se ha podido conectar.");
     } else {
         const auth_token = generateAuthToken();
-        res.send({ status: 'USER_LOGIN_SUCCESS', auth_token: auth_token });
+        console.log("[REST API] El usuario se ha conectado correctamente.");
+        Array.from(user).forEach((u) => {
+            res.send({ status: 'USER_LOGIN_SUCCESS', auth_token: auth_token, password: u.password });
+        })
+
         await Users.updateOne(filter, {
             $set: {
                 token: auth_token
@@ -200,6 +273,7 @@ router.post('/user/update-settings', async(req, res) => {
     };
     const user = await Users.find(filter);
     console.log(user)
+    console.log(req.body)
     if (user.length > 0) {
         res.send({ status: 'UPDATE_SETTINGS_SUCCESS' });
         console.log("[REST API] El perfil se ha actualizado correctamente.");
@@ -209,6 +283,7 @@ router.post('/user/update-settings', async(req, res) => {
                 password: req.body.data.new_password
             }
         });
+        
     } else {
         res.send({ status: 'UPDATE_SETTINGS_FAILED' });
         console.log("[REST API] No se ha podido actualizar el perfil.");
@@ -217,6 +292,7 @@ router.post('/user/update-settings', async(req, res) => {
 
 // END USER - ACCOUNT
 
+// Generates a token for user auth
 const generateAuthToken = () => {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
---
+++ utils/hash.js
@@ -0,0 +1,5 @@
+export utils = {
+    const hash = () => {
+        console.log('test')
+    }
+}
